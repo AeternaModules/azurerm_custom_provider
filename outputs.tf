504794bf1e2cc80667ee3f542078fd0d4a1f911d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "custom_providers" {
-  description = "All custom_provider resources"
-  value       = azurerm_custom_provider.custom_providers
-}
 output "custom_providers_action" {
-  description = "List of action values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.action]
+  description = "Map of action values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.action }
 }
 output "custom_providers_location" {
-  description = "List of location values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.location]
+  description = "Map of location values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.location }
 }
 output "custom_providers_name" {
-  description = "List of name values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.name]
+  description = "Map of name values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.name }
 }
 output "custom_providers_resource_group_name" {
-  description = "List of resource_group_name values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.resource_group_name]
+  description = "Map of resource_group_name values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.resource_group_name }
 }
 output "custom_providers_resource_type" {
-  description = "List of resource_type values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.resource_type]
+  description = "Map of resource_type values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.resource_type }
 }
 output "custom_providers_tags" {
-  description = "List of tags values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.tags]
+  description = "Map of tags values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.tags }
 }
 output "custom_providers_validation" {
-  description = "List of validation values across all custom_providers"
-  value       = [for k, v in azurerm_custom_provider.custom_providers : v.validation]
+  description = "Map of validation values across all custom_providers, keyed the same as var.custom_providers"
+  value       = { for k, v in azurerm_custom_provider.custom_providers : k => v.validation }
 }
 
